47efb7498f76804a0653a65cadcd98444bcd10b2
Florencia zarate <Florenciazarate2110@gmail.com>
2026-04-07 11:55:17 -0300

    style

---
+++ pryEDZarateF/clsArchivoTexto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace pryEDZarateF
+{
+    internal class clsArchivoTexto
+    {
+        public string NombreArchivo = "Colores.csv";
+
+        public void Grabar()
+        {
+            StreamWriter AD = new StreamWriter(NombreArchivo, true);
+            AD.WriteLine();
+            AD.Close();
+        }
+        public void Grabar(String Dato)
+        {
+            StreamWriter AD = new StreamWriter(NombreArchivo, true);
+            AD.WriteLine(Dato);
+            AD.Close();
+        }
+        public void Grabar(String Codigo, String Nombre)
+        {
+            StreamWriter AD = new StreamWriter(NombreArchivo, true);
+            AD.Write(Codigo);
+            AD.Write(";");
+            AD.WriteLine(Nombre);
+            AD.Close();
+        }
+        public void Recorrer(ListBox lstColor)
+        {
+            String DatoLeido;
+            lst.Items.Clear();
+            StreamReader AD = new StreamReader(NombreArchivo);
+            DatoLeido = AD.ReadLine();
+            while (DatoLeido !=null)
+            {
+                lst.Items.Add(DatoLeido);
+                DatoLeido = AD.ReadLine();
+            }
+            AD.Close();
+        }
+
+    }
+}
--- pryEDZarateF/frmColores.Designer.cs
+++ pryEDZarateF/frmColores.Designer.cs
@@ -28,21 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnListar = new MaterialSkin2Framework.Controls.MaterialButton();
+            this.btnGrabar = new MaterialSkin2Framework.Controls.MaterialButton();
+            this.txtNombreColor = new MaterialSkin2Framework.Controls.MaterialTextBox2();
+            this.lblColor = new MaterialSkin2Framework.Controls.MaterialLabel();
+            this.materialLabel1 = new MaterialSkin2Framework.Controls.MaterialLabel();
+            this.lstColor = new MaterialSkin2Framework.Controls.MaterialListBox();
             this.SuspendLayout();
+            // 
+            // btnListar
+            // 
+            this.btnListar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnListar.Density = MaterialSkin2Framework.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnListar.Depth = 0;
+            this.btnListar.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListar.HighEmphasis = true;
+            this.btnListar.Icon = null;
+            this.btnListar.Location = new System.Drawing.Point(103, 121);
+            this.btnListar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnListar.MouseState = MaterialSkin2Framework.MouseState.HOVER;
+            this.btnListar.Name = "btnListar";
+            this.btnListar.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnListar.Size = new System.Drawing.Size(70, 36);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Listar";
+            this.btnListar.Type = MaterialSkin2Framework.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnListar.UseAccentColor = false;
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnGrabar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnGrabar.Density = MaterialSkin2Framework.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnGrabar.Depth = 0;
+            this.btnGrabar.HighEmphasis = true;
+            this.btnGrabar.Icon = null;
+            this.btnGrabar.Location = new System.Drawing.Point(275, 121);
+            this.btnGrabar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnGrabar.MouseState = MaterialSkin2Framework.MouseState.HOVER;
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnGrabar.Size = new System.Drawing.Size(78, 36);
+            this.btnGrabar.TabIndex = 1;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.Type = MaterialSkin2Framework.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnGrabar.UseAccentColor = false;
+            this.btnGrabar.UseVisualStyleBackColor = false;
+            // 
+            // txtNombreColor
+            // 
+            this.txtNombreColor.AnimateReadOnly = false;
+            this.txtNombreColor.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.txtNombreColor.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.txtNombreColor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.txtNombreColor.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.txtNombreColor.Depth = 0;
+            this.txtNombreColor.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreColor.HideSelection = true;
+            this.txtNombreColor.LeadingIcon = null;
+            this.txtNombreColor.Location = new System.Drawing.Point(103, 64);
+            this.txtNombreColor.MaxLength = 32767;
+            this.txtNombreColor.MouseState = MaterialSkin2Framework.MouseState.OUT;
+            this.txtNombreColor.Name = "txtNombreColor";
+            this.txtNombreColor.PasswordChar = '\0';
+            this.txtNombreColor.PrefixSuffixText = null;
+            this.txtNombreColor.ReadOnly = false;
+            this.txtNombreColor.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtNombreColor.SelectedText = "";
+            this.txtNombreColor.SelectionLength = 0;
+            this.txtNombreColor.SelectionStart = 0;
+            this.txtNombreColor.ShortcutsEnabled = true;
+            this.txtNombreColor.Size = new System.Drawing.Size(250, 48);
+            this.txtNombreColor.TabIndex = 3;
+            this.txtNombreColor.TabStop = false;
+            this.txtNombreColor.Text = "Ingrese un color";
+            this.txtNombreColor.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtNombreColor.TrailingIcon = null;
+            this.txtNombreColor.UseSystemPasswordChar = false;
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Depth = 0;
+            this.lblColor.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColor.Location = new System.Drawing.Point(41, 82);
+            this.lblColor.MouseState = MaterialSkin2Framework.MouseState.HOVER;
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(38, 19);
+            this.lblColor.TabIndex = 4;
+            this.lblColor.Text = "Color";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.Location = new System.Drawing.Point(40, 19);
+            this.materialLabel1.MouseState = MaterialSkin2Framework.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(147, 19);
+            this.materialLabel1.TabIndex = 7;
+            this.materialLabel1.Text = "Bienvenido a colores";
+            // 
+            // lstColor
+            // 
+            this.lstColor.BackColor = System.Drawing.Color.DarkOrange;
+            this.lstColor.BorderColor = System.Drawing.Color.DimGray;
+            this.lstColor.Depth = 0;
+            this.lstColor.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.lstColor.Location = new System.Drawing.Point(433, 64);
+            this.lstColor.MouseState = MaterialSkin2Framework.MouseState.HOVER;
+            this.lstColor.Name = "lstColor";
+            this.lstColor.SelectedIndex = -1;
+            this.lstColor.SelectedItem = null;
+            this.lstColor.Size = new System.Drawing.Size(337, 172);
+            this.lstColor.TabIndex = 8;
             // 
             // frmColores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(823, 293);
+            this.Controls.Add(this.lstColor);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.txtNombreColor);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnListar);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmColores";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Colores";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MaterialSkin2Framework.Controls.MaterialButton btnListar;
+        private MaterialSkin2Framework.Controls.MaterialButton btnGrabar;
+        private MaterialSkin2Framework.Controls.MaterialTextBox2 txtNombreColor;
+        private MaterialSkin2Framework.Controls.MaterialLabel lblColor;
+        private MaterialSkin2Framework.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin2Framework.Controls.MaterialListBox lstColor;
     }
 }

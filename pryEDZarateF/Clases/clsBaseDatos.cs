e126e065360edf3452d1459e02772a78293e58a0
Florencia zarate <florenciazarate2110@gmail.com>
2026-05-28 12:05:40 -0300

    new

--- pryEDZarateF/Clases/clsBaseDatos.cs
+++ pryEDZarateF/Clases/clsBaseDatos.cs
@@ -14,6 +14,33 @@ namespace pryEDZarateF.Clases
         private OleDbConnection conexion = new OleDbConnection();
         private OleDbConnection comando = new OleDbConnection();
         private OleDbConnection adaptador = new OleDbConnection();
+        private string CadenaConexion1 = "Provider= Microsoft.Jet.OleDB.4.0;Data Source=Libreria.mb";
+        private string CadenaConexion2 = "Provider= Microsoft.Jet.OleDB.12.0;Data Source=Libreria.mb";
+        public void Listar(DataGridView Grilla)
+        {
+            try
+            {
+                conexion.ConnectionString = CadenaConexion1;
+                conexion.Open();
+                comando.ConnectionString = conexion;
+                comando.CommandType = CommandType.TableDirect;
+                comando.CommandText = "Libro";
 
-    }
-}
+                DataSet DS = new DataSet();
+                adaptador = new OleDbDataAdapter(comando);
+                adaptador.Fill(DS, "Libro");
+
+                Grilla.DataSource = null;
+                Grilla.DataSource = DS.Tables["Libro"];
+
+                conexion.Close();
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+
+
+
+

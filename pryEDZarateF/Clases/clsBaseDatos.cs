e528b098c95826f4f378857c7de08de0fff2ac04
Florencia zarate <florenciazarate2110@gmail.com>
2026-05-28 11:31:36 -0300

    new clsBaseDatos

---
+++ pryEDZarateF/Clases/clsBaseDatos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace pryEDZarateF.Clases
+{
+    internal class clsBaseDatos
+    {
+        private OleDbConnection conexion = new OleDbConnection();
+        private OleDbConnection comando = new OleDbConnection();
+        private OleDbConnection adaptador = new OleDbConnection();
+
+    }
+}
